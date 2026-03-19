68af71cc97cf354a2114fcbcc7562ac0446de5fa
DevEdu <quisperojasedu@gmail.com>
2026-03-19 01:56:14 -0500

    fix: actualizacion de configuracion de seguridad compatible con Spring Security moderno

--- src/main/java/com/edudev/config/SecurityConfig.java
+++ src/main/java/com/edudev/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.edudev.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,16 +7,18 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    //capa de seguridad
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
-                .csrf(csrf -> csrf.disable()) // IMPORTANTE para POST
+                .cors(cors -> {}) // 👈 IMPORTANTE
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
 
         return http.build();
     }
+
+
 }
